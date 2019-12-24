712fbbb4400a9a5042994a80976769242b0c69b5
David Lennon <david.lennon@libertyits.com>
2019-12-24 15:32:27 -0500

    Start of integration test - still failing because Database is not properly setup yet - in progress

---
+++ fall-risk-tests/src/test/java/gov/va/api/health/fallrisk/tests/FallRiskIT.java
@@ -0,0 +1,26 @@
+package gov.va.api.health.fallrisk.tests;
+
+import gov.va.api.health.fallrisk.tests.categories.LabFallRisk;
+import gov.va.api.health.fallrisk.tests.categories.ProdFallRisk;
+import gov.va.api.health.sentinel.ExpectedResponse;
+import gov.va.api.health.sentinel.categories.Local;
+import io.restassured.http.Method;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class FallRiskIT {
+
+  @Test
+  @Category({Local.class, ProdFallRisk.class, LabFallRisk.class})
+  public void searchByPatient() {
+    ExpectedResponse.of(
+            TestClients.fallRisk()
+                .service()
+                .requestSpecification()
+                .contentType("application/json")
+                .request(
+                    Method.GET,
+                    TestClients.fallRisk().service().urlWithApiPath() + "?patient=12345V67890"))
+        .expect(500);
+  }
+}

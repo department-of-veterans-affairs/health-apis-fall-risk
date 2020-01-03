5cb4f5270ec5575766995aadecd855374499c464
David Lennon <david.lennon@libertyits.com>
2020-01-03 14:02:14 -0500

    Added Prod integration tests

--- fall-risk-tests/src/test/java/gov/va/api/health/fallrisk/tests/FallRiskIT.java
+++ fall-risk-tests/src/test/java/gov/va/api/health/fallrisk/tests/FallRiskIT.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import gov.va.api.health.fallrisk.service.controller.FallRiskAssessmentResponse;
 import gov.va.api.health.fallrisk.tests.categories.LabFallRisk;
+import gov.va.api.health.fallrisk.tests.categories.ProdFallRisk;
 import gov.va.api.health.sentinel.ExpectedResponse;
 import gov.va.api.health.sentinel.categories.Local;
 import io.restassured.http.Method;
@@ -32,6 +33,21 @@ public class FallRiskIT {
   }
 
   @Test
+  @Category({ProdFallRisk.class})
+  public void searchByFacilityAndSinceForProd() {
+    ExpectedResponse.of(
+            TestClients.fallRisk()
+                .service()
+                .requestSpecification()
+                .contentType("application/json")
+                .request(
+                    Method.GET,
+                    TestClients.fallRisk().service().urlWithApiPath()
+                        + "assessment?facility=640&since=1200000000"))
+        .expect(500);
+  }
+
+  @Test
   @Category({Local.class, LabFallRisk.class})
   public void searchByPatient() {
     FallRiskAssessmentResponse response =
@@ -49,5 +65,20 @@ public class FallRiskIT {
     assertThat(response).isNotNull();
     assertThat(response.getFacilityId()).isEqualTo("640");
     assertThat(response.getPatient()).isEqualTo("43000199");
+  }
+
+  @Test
+  @Category({ProdFallRisk.class})
+  public void searchByPatientForProd() {
+    ExpectedResponse.of(
+            TestClients.fallRisk()
+                .service()
+                .requestSpecification()
+                .contentType("application/json")
+                .request(
+                    Method.GET,
+                    TestClients.fallRisk().service().urlWithApiPath()
+                        + "assessment?patient=43000199"))
+        .expect(500);
   }
 }

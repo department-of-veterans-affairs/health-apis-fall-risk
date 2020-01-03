2cf0ffc2fe8fd1e4282be5e4938d0758ff5189e6
David Lennon <david.lennon@libertyits.com>
2020-01-03 13:27:54 -0500

    Make integration tests actually look for mitre data.

--- fall-risk-tests/src/test/java/gov/va/api/health/fallrisk/tests/FallRiskIT.java
+++ fall-risk-tests/src/test/java/gov/va/api/health/fallrisk/tests/FallRiskIT.java
@@ -1,44 +1,52 @@
 package gov.va.api.health.fallrisk.tests;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import gov.va.api.health.fallrisk.service.controller.FallRiskAssessmentResponse;
 import gov.va.api.health.fallrisk.tests.categories.LabFallRisk;
-import gov.va.api.health.fallrisk.tests.categories.ProdFallRisk;
 import gov.va.api.health.sentinel.ExpectedResponse;
 import gov.va.api.health.sentinel.categories.Local;
 import io.restassured.http.Method;
-import java.time.Instant;
+import java.util.List;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 public class FallRiskIT {
 
   @Test
-  @Category({Local.class, ProdFallRisk.class, LabFallRisk.class})
+  @Category({Local.class, LabFallRisk.class})
   public void searchByFacilityAndSince() {
-    ExpectedResponse.of(
-            TestClients.fallRisk()
-                .service()
-                .requestSpecification()
-                .contentType("application/json")
-                .request(
-                    Method.GET,
-                    TestClients.fallRisk().service().urlWithApiPath()
-                        + "assessment?facility=640&since="
-                        + Instant.now().toEpochMilli()))
-        .expect(500);
+    List<FallRiskAssessmentResponse> response =
+        ExpectedResponse.of(
+                TestClients.fallRisk()
+                    .service()
+                    .requestSpecification()
+                    .contentType("application/json")
+                    .request(
+                        Method.GET,
+                        TestClients.fallRisk().service().urlWithApiPath()
+                            + "assessment?facility=640&since=1200000000"))
+            .expect(200)
+            .expectValid(List.class);
+    assertThat(response.size()).isEqualTo(2);
   }
 
   @Test
-  @Category({Local.class, ProdFallRisk.class, LabFallRisk.class})
+  @Category({Local.class, LabFallRisk.class})
   public void searchByPatient() {
-    ExpectedResponse.of(
-            TestClients.fallRisk()
-                .service()
-                .requestSpecification()
-                .contentType("application/json")
-                .request(
-                    Method.GET,
-                    TestClients.fallRisk().service().urlWithApiPath()
-                        + "assessment?patient=12345V67890"))
-        .expect(500);
+    FallRiskAssessmentResponse response =
+        ExpectedResponse.of(
+                TestClients.fallRisk()
+                    .service()
+                    .requestSpecification()
+                    .contentType("application/json")
+                    .request(
+                        Method.GET,
+                        TestClients.fallRisk().service().urlWithApiPath()
+                            + "assessment?patient=43000199"))
+            .expect(200)
+            .expectValid(FallRiskAssessmentResponse.class);
+    assertThat(response).isNotNull();
+    assertThat(response.getFacilityId()).isEqualTo("640");
   }
 }

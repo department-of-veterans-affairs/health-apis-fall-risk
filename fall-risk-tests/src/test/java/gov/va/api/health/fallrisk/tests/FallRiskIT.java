014e0aa35853729aa3b9f9de405b1b3d1aa16078
eclendenning-va <evan.clendenning@libertyits.com>
2020-02-07 16:18:19 -0500

    added integration tests for both paths

--- fall-risk-tests/src/test/java/gov/va/api/health/fallrisk/tests/FallRiskIT.java
+++ fall-risk-tests/src/test/java/gov/va/api/health/fallrisk/tests/FallRiskIT.java
@@ -16,7 +16,6 @@ import org.junit.experimental.categories.Category;
 
 @Slf4j
 public class FallRiskIT {
-
   private String apiPath() {
     return TestClients.fallRisk().service().apiPath();
   }
@@ -28,6 +27,82 @@ public class FallRiskIT {
   @Test
   @Category({Local.class, LabFallRisk.class, ProdFallRisk.class})
   public void searchByFacilityAndSince() {
+    final String facility = systemDefinition().testIds().station();
+    final String lastUpdated = systemDefinition().testIds().since();
+    log.info(
+        "Verify {}v0/assessment?facility={}&since={} is list of FallRiskAssessmentResponse (200)",
+        apiPath(),
+        facility,
+        lastUpdated);
+    List<FallRiskAssessmentResponse> response =
+        ExpectedResponse.of(
+                TestClients.fallRisk()
+                    .service()
+                    .requestSpecification()
+                    .header(fallRiskToken())
+                    .contentType("application/json")
+                    .request(
+                        Method.GET,
+                        TestClients.fallRisk().service().urlWithApiPath()
+                            + "v0/assessment?facility="
+                            + facility
+                            + "&since="
+                            + lastUpdated))
+            .expect(200)
+            .expectListOf(FallRiskAssessmentResponse.class);
+    assertThat(response).isNotEmpty();
+  }
+
+  @Test
+  @Category({Local.class, LabFallRisk.class, ProdFallRisk.class})
+  public void searchByFacilityAndSinceReturnsBadRequestForUnparseableDate() {
+    log.info("Verify {}v0/assessment?facility=640&since=IDKMYBFFJILL is status (400)", apiPath());
+    ExpectedResponse.of(
+            TestClients.fallRisk()
+                .service()
+                .requestSpecification()
+                .header(fallRiskToken())
+                .contentType("application/json")
+                .request(
+                    Method.GET,
+                    TestClients.fallRisk().service().urlWithApiPath()
+                        + "v0/assessment?facility=640&since=IDKMYBFFJILL"))
+        .expect(400);
+  }
+
+  @Test
+  @Category({Local.class, LabFallRisk.class, ProdFallRisk.class})
+  public void searchByPatient() {
+    final String patientFullIcn = systemDefinition().testIds().patient();
+    log.info(
+        "Verify {}v0/assessment?patient={} is list of FallRiskAssessmentResponse (200)",
+        apiPath(),
+        patientFullIcn);
+    List<FallRiskAssessmentResponse> response =
+        ExpectedResponse.of(
+                TestClients.fallRisk()
+                    .service()
+                    .requestSpecification()
+                    .header(fallRiskToken())
+                    .contentType("application/json")
+                    .request(
+                        Method.GET,
+                        TestClients.fallRisk().service().urlWithApiPath()
+                            + "v0/assessment?patient="
+                            + patientFullIcn))
+            .expect(200)
+            .expectListOf(FallRiskAssessmentResponse.class);
+    assertThat(response).isNotEmpty();
+    assertThat(response.get(0).patient()).isEqualTo(systemDefinition().testIds().patient());
+  }
+
+  private SystemDefinition systemDefinition() {
+    return SystemDefinitions.systemDefinition();
+  }
+
+  @Test
+  @Category({Local.class, LabFallRisk.class, ProdFallRisk.class})
+  public void unversionedSearchByFacilityAndSince() {
     final String facility = systemDefinition().testIds().station();
     final String lastUpdated = systemDefinition().testIds().since();
     log.info(
@@ -51,13 +126,12 @@ public class FallRiskIT {
                             + lastUpdated))
             .expect(200)
             .expectListOf(FallRiskAssessmentResponse.class);
-
     assertThat(response).isNotEmpty();
   }
 
   @Test
   @Category({Local.class, LabFallRisk.class, ProdFallRisk.class})
-  public void searchByFacilityAndSinceReturnsBadRequestForUnparseableDate() {
+  public void unversionedSearchByFacilityAndSinceReturnsBadRequestForUnparseableDate() {
     log.info("Verify {}assessment?facility=640&since=IDKMYBFFJILL is status (400)", apiPath());
     ExpectedResponse.of(
             TestClients.fallRisk()
@@ -74,7 +148,7 @@ public class FallRiskIT {
 
   @Test
   @Category({Local.class, LabFallRisk.class, ProdFallRisk.class})
-  public void searchByPatient() {
+  public void unversionedSearchByPatient() {
     final String patientFullIcn = systemDefinition().testIds().patient();
     log.info(
         "Verify {}assessment?patient={} is list of FallRiskAssessmentResponse (200)",
@@ -96,9 +170,5 @@ public class FallRiskIT {
             .expectListOf(FallRiskAssessmentResponse.class);
     assertThat(response).isNotEmpty();
     assertThat(response.get(0).patient()).isEqualTo(systemDefinition().testIds().patient());
-  }
-
-  private SystemDefinition systemDefinition() {
-    return SystemDefinitions.systemDefinition();
   }
 }

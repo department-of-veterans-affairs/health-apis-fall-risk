08a6a1cbccde383046dfc7193441e62e9f5a1d33
Evan Clendenning <evan.clendenning@libertyits.com>
2020-02-13 10:49:25 -0500

    Fall Risk upgrade to parent 6.0.0 (#21)

--- fall-risk/src/main/java/gov/va/api/health/fallrisk/service/controller/FallRiskAssessmentController.java
+++ fall-risk/src/main/java/gov/va/api/health/fallrisk/service/controller/FallRiskAssessmentController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(
     value = {"/assessment", "v0/assessment"},
     produces = {"application/json"})
-@Slf4j
 @Builder
 @Loggable
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
